d22a9ecbad445af8e50e6e5f17e2c1639dcca1a8
josemar9993 <schieste87@gmail.com>
2025-06-24 07:03:25 -0300

    fix: add single-process arg to puppeteer

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ const client = new Client({
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
         '--no-zygote',
+        '--single-process',
         '--disable-gpu'
       ]
     };
