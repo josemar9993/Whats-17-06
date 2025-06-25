47b8cf856ee9f33e93daab21be59ce5ab6d214c8
josemar9993 <schieste87@gmail.com>
2025-06-25 07:39:08 -0300

    chore: remover arquivos de PM2 e Cloud Build

--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps: [{
-    name: 'meu-bot-whatsapp',
-    script: './src/index.js', // Caminho para o script principal
-    watch: false,
-    max_memory_restart: '500M'
-  }]
-};
